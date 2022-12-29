8c6d47bf15107ef601e4201e56c0fd47a07cb9d0
Elis Hirwing <elis@hirwing.se>
2022-12-29 15:03:11 +0100

    feat(main): Add prompting before executing ffmpeg on files

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,24 +27,42 @@ func main() {
 		}
 	}
 
+	// Count all the files
+	fileCount := len(fileNames)
+	fileNo := 0
+
 	for _, fileName := range fileNames {
+		fileNo++
+
 		var ffprobe FFProbe
 
 		ffprobe.Identify(fileName)
 
-		ffmpeg := FFMpeg{
-			inputFilePath:  fileName,
-			audioTracks:    ffprobe.GetAudioTracks([]string{"eng"}),
-			subtitleTracks: ffprobe.GetSubtitleTracks([]string{"eng"}),
-			videoTracks:    ffprobe.GetVideoTracks(),
+		fmt.Printf("[%d/%d] Preparing to process %s\n", fileNo, fileCount, fileName)
+		fmt.Printf("[%d/%d] Run ffmpeg command on %s? [Y/n] ", fileNo, fileCount, fileName)
+
+		reader := bufio.NewReader(os.Stdin)
+		response, _ := reader.ReadString('\n')
+		response = strings.TrimSpace(response)
+
+		if strings.ToLower(response) == "y" || response == "" {
+			ffmpeg := FFMpeg{
+				inputFilePath:  fileName,
+				audioTracks:    ffprobe.GetAudioTracks([]string{"eng"}),
+				subtitleTracks: ffprobe.GetSubtitleTracks([]string{"eng"}),
+				videoTracks:    ffprobe.GetVideoTracks(),
+			}
+
+			fmt.Printf("[%d/%d] Executing: %s\n", fileNo, fileCount, ffmpeg.FormatCommandParts())
+			err := ffmpeg.Run()
+			if err == nil {
+				fmt.Printf("[%d/%d] Sucessfully cleaned up: %s\n", fileNo, fileCount, fileName)
+			} else {
+				fmt.Printf("[%d/%d] Failed at cleaning up: %s\n", fileNo, fileCount, fileName)
+				fmt.Printf("[%d/%d] Error: %s\n", fileNo, fileCount, err)
+			}
+		} else {
+			fmt.Printf("[%d/%d] Skipping %s\n", fileNo, fileCount, fileName)
 		}
-
-		fmt.Println("ffmpeg struct:", ffmpeg)
-		fmt.Println("ffmpeg command:", ffmpeg.FormatCommandParts())
-
-		fmt.Println("ffmpeg running:")
-		success, err := ffmpeg.Run()
-		fmt.Println("ffmpeg success:", success)
-		fmt.Println("ffmpeg err:", err)
 	}
 }
